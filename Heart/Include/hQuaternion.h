98fd236b8121395803d2f8c0addb4839cd3777f9
James Moran <jojo.2nd.jm@googlemail.com>
2011-12-22 10:43:29 +0000

    Moved hQuaternion to xnamath library space police

--- Heart/Include/hQuaternion.h
+++ Heart/Include/hQuaternion.h
@@ -12,346 +12,124 @@
 #include "hTypes.h"
 #include "hDebugMacros.h"
 #include <float.h>
+#include <xnamath.h>
 
 namespace Heart
 {
 
-	//-----------------------------------------------------------------------
-	///
-	/// @class Quaternion
-	/// @brief 
-	/// @author James Moran
-	/// @date [13/7/2008]
-	/// @version 1.0
-	///
-	//-----------------------------------------------------------------------
-	class hQuaternion
+    typedef XMVECTOR hQuaternion;
+    typedef XMFLOAT4 hCPUQuaternion;
+
+namespace hQuaternionFunc
+{
+    hFORCEINLINE hQuaternion&	identity()
 	{
-	public:
-		union
-		{
-			hFloat q[ 4 ];
-			struct
-			{
-				hFloat w;
-				hFloat x;
-				hFloat y;
-				hFloat z;
-			};
-		};
+        return XMQuaternionIdentity();
+	}
 
-		hQuaternion() 
-		{
+	hFORCEINLINE hQuaternion&	RotateAxis( hFloat t, const hVec3& axis )
+	{
+        return XMQuaternionRotationAxis( axis, t );
+	}
 
-		}
+	hFORCEINLINE hQuaternion&	rotateX( hFloat t )
+	{
+        return RotateAxis( t, hCPUVec3( 1.f, 0.f, 0.f ) );
+	}
 
-		hQuaternion( hFloat cw, hFloat cx, hFloat cy, hFloat cz ) :
-			w( cw ),
-			x( cx ),
-			y( cy ),
-			z( cz )
-		{
+    hFORCEINLINE hQuaternion&	rotateY( hFloat t )
+    {
+        return RotateAxis( t, hCPUVec3( 0.f, 1.f, 0.f ) );
+    }
 
-		}
+    hFORCEINLINE hQuaternion&	rotateZ( hFloat t )
+    {
+        return RotateAxis( t, hCPUVec3( 0.f, 0.f, 1.f ) );
+    }
 
-		/**
-		* 
-		*
-		* @return   	void
-		*/
-		void							Identity();
-		/**
-		* 
-		*
-		* @param 		dst
-		* @return   	Quaternion*
-		*/
-		static hQuaternion&			identity( hQuaternion& dst )
-		{
-			dst.w = 1.0f;
-			dst.x = 0.0f;
-			dst.y = 0.0f;
-			dst.z = 0.0f;
+    hFORCEINLINE hQuaternion& rotate( hFloat x, hFloat y, hFloat z )
+    {
+        return XMQuaternionRotationRollPitchYaw( x, y, z );
+    }
 
-			return dst;
-		}
+    hFORCEINLINE hQuaternion& rotate( const hVec3& v )
+    {
+        return XMQuaternionRotationRollPitchYawFromVector( v );
+    }
 
-		static hQuaternion&				RotateAxis( hFloat x, const hVec3& axis, hQuaternion& dst )
-		{
-			hFloat theta = x * 0.5f;
+	hFORCEINLINE hQuaternion& mult( const hQuaternion& a, const hQuaternion& b )
+	{
+        return XMQuaternionMultiply( a, b );
+	}
 
-			hVec3 a;
-			hVec3::normalise( axis, a );
+    hFORCEINLINE hQuaternion& normalise( const hQuaternion& a)
+	{
+        return XMQuaternionNormalize( a );
+	}
 
-			dst.w = cos( theta );
-			dst.x = sin( theta )*a.x;
-			dst.y = sin( theta )*a.y;
-			dst.z = sin( theta )*a.z;
+    hFORCEINLINE hQuaternion& normaliseFast( const hQuaternion& a)
+    {
+        return XMQuaternionNormalize( a );
+    }
 
-			return dst;
-		}
+	hFORCEINLINE hFloat length( const hQuaternion& a )
+	{
+		return XMQuaternionLength( a );
+	}
 
-		/**
-		* 
-		*
-		* @param 		x
-		* @param 		dst
-		* @return   	void
-		*/
-		static hQuaternion&				rotateX( hFloat x, hQuaternion& dst )
-		{
-			hFloat theta = x * 0.5f;
+    hFORCEINLINE hFloat lengthSquare( const hQuaternion& a )
+    {
+        return XMQuaternionLengthSq( a );
+    }
 
-			dst.w = cos( theta );
-			dst.x = sin( theta );
-			dst.y = 0.0f;
-			dst.z = 0.0f;
+    hFORCEINLINE hFloat dot( const hQuaternion& a, const hQuaternion& b )
+	{
+		return XMQuaternionDot( a, b );
+	}
 
-			return dst;
-		}
+	hFORCEINLINE hQuaternion& slerp( const hQuaternion& a, const hQuaternion& b, hFloat dt )
+	{
+        return XMQuaternionSlerp( a, b, dt );
+	}
 
-		static hQuaternion&				rotateY( hFloat y, hQuaternion& dst )
-		{
-			hFloat theta = y * 0.5f;
+	hFORCEINLINE hQuaternion& conjugate( hQuaternion& a )
+	{
+        return XMQuaternionConjugate( a );
+	}
 
-			dst.w = cos( theta );
-			dst.x = 0.0f;
-			dst.y = sin( theta );
-			dst.z = 0.0f;
+	hFORCEINLINE hQuaternion& decompress( hFloat x, hFloat y, hFloat z )
+	{
+		hFloat w = 1.0f - (x * x) - (y * y) - (z * z);
+		w = sqrt( fabs( w ) );
+		return normalise( hCPUQuaternion( w, x, y, z ) );
+	}
 
-			return dst;
-		}
+    hFORCEINLINE hBool IsIdentity( const hQuaternion& q )
+	{
+		return XMQuaternionIsIdentity( q );
+	}
 
-		static hQuaternion&				rotateZ( hFloat z, hQuaternion& dst )
-		{
-			hFloat theta = z * 0.5f;
+    hFORCEINLINE void store( const hQuaternion& a, hCPUQuaternion* b )
+    {
+        XMStoreFloat4( b, a );
+    }
 
-			dst.w = cos( theta );
-			dst.x = 0.0f;
-			dst.y = 0.0f;
-			dst.z = sin( theta );
+    hFORCEINLINE void load( const hQuaternion& a, hCPUQuaternion* b )
+    {
+        a = XMLoadFloat4( b );
+    }
+}
 
-			return dst;
-		}
+    hFORCEINLINE hCPUQuaternion& operator = ( const hCPUQuaternion& a, const hQuaternion& b )
+    {
+        hQuaternionFunc::store( b, &a );
+    }
 
-		/**
-		* TODO: fix so rotation order is XYZ
-		*
-		* @param 		x
-		* @param 		y
-		* @param 		z
-		* @param 		dst
-		* @return   	Quaternion*
-		*/
-		static hQuaternion&			rotate( hFloat x, hFloat y, hFloat z, hQuaternion& dst )
-		{
-			hFloat sx, sy, sz;
-			hFloat cx, cy, cz;
-			hFloat x2 = x * 0.5f;
-			hFloat y2 = y * 0.5f;
-			hFloat z2 = z * 0.5f;
-
-			sx = sin( x2 ); cx = cos( x2 );
-			sy = sin( y2 ); cy = cos( y2 );
-			sz = sin( z2 ); cz = cos( z2 );
-
-	//  		dst.w = ( cy * cx * cz ) + ( sy * sx * sz );
-	//  		dst.x = ( cy * sx * cz ) + ( sy * cx * sz );
-	//  		dst.y = ( -cy * sx * sz ) + ( sy * cx * cz );
-	//  		dst.z = ( -sy * sx * cz ) + ( cy * cx * sz );
-
- 			dst.w = ( cx * cy * cz ) - ( sx * sy * sz );
- 			dst.x = ( sx * cy * cz ) + ( cx * sy * sz );
- 			dst.y = ( cx * sy * cz ) - ( sx * cy * sz );
- 			dst.z = ( cx * cy * sz ) + ( sx * sy * cz );
-
-			return dst;
-		}
-
-		static hQuaternion&			mult( const hQuaternion& a, const hQuaternion& b, hQuaternion& c )
-		{
-			hQuaternion ret;
-			hQuaternion* o = &c;
-			if ( &a == &c || &b == &c )
-			{
-				o = &ret;
-			}
-
-	// 		o->w = ( a.w * b.w ) - ( a.x * b.x ) - ( a.y * b.y ) - ( a.z * b.z );
-	// 		o->x = ( a.w * b.x ) + ( a.x * b.w ) + ( a.z * b.y ) - ( a.y * b.z );
-	// 		o->y = ( a.w * b.y ) + ( a.y * b.w ) + ( a.x * b.z ) - ( a.z * b.x );
-	// 		o->z = ( a.w * b.z ) + ( a.z * b.w ) + ( a.y * b.x ) - ( a.x * b.y );
-
-			o->w = ( a.w * b.w ) - ( a.x * b.x ) - ( a.y * b.y ) - ( a.z * b.z );
-			o->x = ( a.w * b.x ) + ( a.x * b.w ) + ( a.y * b.z ) - ( a.z * b.y );
-			o->y = ( a.w * b.y ) + ( a.y * b.w ) + ( a.z * b.x ) - ( a.x * b.z );
-			o->z = ( a.w * b.z ) + ( a.z * b.w ) + ( a.x * b.y ) - ( a.y * b.x );
-
-
-			if ( o == &ret )
-			{
-				c = *o;
-			}
-
-			return c;
-		}
-
-		/**
-		* 
-		*
-		* @param 		a
-		* @return   	void
-		*/
-		void							operator * ( const hQuaternion& a );
-
-		bool							operator == ( const hQuaternion& b )
-		{
-			return ( x == b.x && y == b.y && z == b.z && w == b.w );
-		}
-
-		/**
-		* 
-		*
-		* @return   	void
-		*/
-		void							Normalise();
-		/**
-		* 
-		*
-		* @param 		a
-		* @param 		norm
-		* @return   	Quaternion*
-		*/
-		static hQuaternion&			normalise( const hQuaternion& a, hQuaternion& norm )
-		{
-			hFloat mag = sqrt( ( a.w * a.w ) + ( a.x * a.x ) + ( a.y * a.y ) + ( a.z * a.z ) );
-
-			hcAssert( mag > 0.0f );
-			
-			if ( mag > 0.0f )
-			{
-				float d = 1.0f / mag;
-				norm.w = a.w * d; 
-				norm.x = a.x * d;
-				norm.y = a.y * d;
-				norm.z = a.z * d;
-			}
-
-			return norm;
-		}
-
-		static hFloat					length( const hQuaternion& a )
-		{
-			return sqrt( ( a.w * a.w ) + ( a.x * a.x ) + ( a.y * a.y ) + ( a.z * a.z ) );
-		}
-
-		/**
-		* 
-		*
-		* @param 		a
-		* @param 		b
-		* @return   	hFloat
-		*/
-		static hFloat					dot( const hQuaternion& a, const hQuaternion& b )
-		{
-			return ( a.w * b.w ) + ( a.x * b.x ) + ( a.y * b.y ) + ( a.z * b.y );
-		}
-
-		static hQuaternion&			slerp( const hQuaternion& a, const hQuaternion& b, hFloat dt, hQuaternion& out )
-		{
-			if ( dt <= 0.0f )
-			{
-				out.w = a.w;
-				out.x = a.x;
-				out.y = a.y;
-				out.z = a.z;
-
-				return out;
-			}
-			if ( dt >= 1.0f )
-			{
-				out.w = b.w;
-				out.x = b.x;
-				out.y = b.y;
-				out.z = b.z;
-
-				return out;
-			}
-
-			hFloat cosomega = hQuaternion::dot( a, b );
-
-			if ( fabs( cosomega ) == 1.0f )
-			{
-				//quaternions are the same or not normalised, return a
-				out = a;
-				return out;
-			}
-
-			hFloat bw = b.w;
-			hFloat bx = b.x;
-			hFloat by = b.y;
-			hFloat bz = b.z;
-
-			// if cosomega is < 0 angle between a & b is obtuse and we a the acute one [11/27/2008 James]
-			if ( cosomega < 0.0f )
-			{
-				cosomega = -cosomega;
-				bw = -bw;
-				bx = -bx;
-				by = -by;
-				bz = -bz;
-			}
-
-			// check quaternions are unit [11/27/2008 James]
-			hcAssert( cosomega < 1.1f );
-
-			hFloat k0,k1;
-
-			// use sin^2 + cos^2 == 1 to get sin of angle [11/27/2008 James]
-			hFloat sinomega = sqrt( 1.0f - ( cosomega * cosomega ) );
-			hFloat omega = atan2( sinomega, cosomega );//acos( cosomega );
-			hFloat oneoversin = 1.0f / sinomega;
-
-			// get the lerping values [11/27/2008 James]
-			k0 = sin( ( 1.0f - dt ) * omega ) * oneoversin;
-			k1 = sin( dt * omega ) * oneoversin;
-
-			out.w = ( k0 * a.w ) + ( k1 * bw );
-			out.x = ( k0 * a.x ) + ( k1 * bx );
-			out.y = ( k0 * a.y ) + ( k1 * by );
-			out.z = ( k0 * a.z ) + ( k1 * bz );
-
-			normalise( out, out );
-
-			return out;
-		}
-
-		static hQuaternion& conjugate( hQuaternion& a, hQuaternion& out )
-		{
-			out.w = a.w;
-
-			out.x = -a.x;
-			out.y = -a.y;
-			out.z = -a.z;
-
-			return out;
-		}
-
-		static hQuaternion build( hFloat x, hFloat y, hFloat z )
-		{
-			hFloat w = 1.0f - (x * x) - (y * y) - (z * z);
- 			w = sqrt( fabs( w ) );
-			hQuaternion q;
-			hQuaternion::normalise( hQuaternion ( w, x, y, z ), q );
-			return q;
-		}
-
-		static hBool IsIdentity( const hQuaternion& q )
-		{
-			return q.w == 1.0f && q.x == 0.0f && q.y == 0.0f && q.z == 0.0f;
-		}
-	};
-
+    hFORCEINLINE hQuaternion& operator = ( const hQuaternion& a, const hCPUQuaternion& b )
+    {
+        hQuaternionFunc::load( a, &b );
+    }
+    
 }
 
 #endif // HMQUATERNION_H__
--- Heart/Include/hVec4.h
+++ Heart/Include/hVec4.h
@@ -20,90 +20,90 @@ namespace Heart
     typedef XMVECTOR hVec4;
     typedef XMFLOAT4 hCPUVec4;
 
-    namespace hVec4Func
+namespace hVec4Func
+{
+    //functions
+    hFORCEINLINE hFloat length( const hVec4& v )
     {
-        //functions
-        hFORCEINLINE hFloat length( const hVec4& v )
-        {
-            return XMVector4Length( v ); 
-        }
-
-        hFORCEINLINE hFloat lengthFast( const hVec4& v )
-        {
-            return XMVector4LengthEst( v );
-        }
-
-        hFORCEINLINE hFloat lengthSquare( const hVec4& v ) const
-        {
-            return XMVector4Length( v ); 
-        }
-
-        hFORCEINLINE hVec4& normalise( const hVec4& v )
-        {
-            return XMVector4Normalize( v );
-        }
-
-        hFORCEINLINE hVec4& normaliseFast( const hVec4& v )
-        {
-            return XMVector4NormalizeEst( v );
-        }
-
-        hFORCEINLINE hVec4& add( const hVec4& a, const hVec4& b )
-        {
-            return XMVectorAdd( a, b );
-        }
-
-        hFORCEINLINE hVec4& sub( const hVec4& a, const hVec4& b, hVec4& out )
-        {
-            return XMVectorSubtract( a, b );
-        }
-
-        hFORCEINLINE hVec4& scale( const hVec4& a, const hFloat s )
-        {
-            return XMVectorScale( a, s );
-        }
-
-        hFORCEINLINE hVec4& div( const hVec4& a, const hFloat d )
-        {
-            return XMVectorDivide( a, d );
-        }
-
-        hFORCEINLINE hFloat dot( const hVec4& a, const hVec4& b )
-        {
-            return XMVector4Dot( a, b );
-        }
-
-        hFORCEINLINE hVec4& neg( const hVec4& a )
-        {
-            return XMVectorNegate( a );
-        }
-
-        hFORCEINLINE hVec4& cross( const hVec4& a, const hVec4& b, const hVec4& c )
-        {
-            return XMVector4Cross( a, b, c );
-        }
-
-        hFORCEINLINE hVec4& set( hFloat x, hFloat y, hFloat z )
-        {
-            return XMVectorSet( x, y, z, 1.f );
-        }
-
-        hFORCEINLINE hBool compare( const hVec4& a, const hVec4& b )
-        {
-            return XMVector4Equal( a, b );
-        }
-
-        hFORCEINLINE void store( const hVec4& a, hCPUVec4* b )
-        {
-            XMStoreFloat3( b, a );
-        }
-
-        hFORCEINLINE void load( const hVec4& a, hCPUVec4* b )
-        {
-            a = XMLoadFloat3( b );
-        }
-
+        return XMVector4Length( v ); 
     }
+
+    hFORCEINLINE hFloat lengthFast( const hVec4& v )
+    {
+        return XMVector4LengthEst( v );
+    }
+
+    hFORCEINLINE hFloat lengthSquare( const hVec4& v ) const
+    {
+        return XMVector4Length( v ); 
+    }
+
+    hFORCEINLINE hVec4& normalise( const hVec4& v )
+    {
+        return XMVector4Normalize( v );
+    }
+
+    hFORCEINLINE hVec4& normaliseFast( const hVec4& v )
+    {
+        return XMVector4NormalizeEst( v );
+    }
+
+    hFORCEINLINE hVec4& add( const hVec4& a, const hVec4& b )
+    {
+        return XMVectorAdd( a, b );
+    }
+
+    hFORCEINLINE hVec4& sub( const hVec4& a, const hVec4& b, hVec4& out )
+    {
+        return XMVectorSubtract( a, b );
+    }
+
+    hFORCEINLINE hVec4& scale( const hVec4& a, const hFloat s )
+    {
+        return XMVectorScale( a, s );
+    }
+
+    hFORCEINLINE hVec4& div( const hVec4& a, const hFloat d )
+    {
+        return XMVectorDivide( a, d );
+    }
+
+    hFORCEINLINE hFloat dot( const hVec4& a, const hVec4& b )
+    {
+        return XMVector4Dot( a, b );
+    }
+
+    hFORCEINLINE hVec4& neg( const hVec4& a )
+    {
+        return XMVectorNegate( a );
+    }
+
+    hFORCEINLINE hVec4& cross( const hVec4& a, const hVec4& b, const hVec4& c )
+    {
+        return XMVector4Cross( a, b, c );
+    }
+
+    hFORCEINLINE hVec4& set( hFloat x, hFloat y, hFloat z )
+    {
+        return XMVectorSet( x, y, z, 1.f );
+    }
+
+    hFORCEINLINE hBool compare( const hVec4& a, const hVec4& b )
+    {
+        return XMVector4Equal( a, b );
+    }
+
+    hFORCEINLINE void store( const hVec4& a, hCPUVec4* b )
+    {
+        XMStoreFloat4( b, a );
+    }
+
+    hFORCEINLINE void load( const hVec4& a, hCPUVec4* b )
+    {
+        a = XMLoadFloat4( b );
+    }
+
+}
 
     hFORCEINLINE hVec4& operator + ( const hVec4& a, const hVec4& b )
     {
