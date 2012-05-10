24aa93b634f2c2894f2cbaf122097ea623d5c9c3
James Moran <jojo.2nd.jm@googlemail.com>
2012-05-10 19:32:08 +0200

    Extra Defs

--- libtga/include/libtga.h
+++ libtga/include/libtga.h
@@ -1,7 +1,8 @@
-
 typedef unsigned char   tga_byte;
 typedef short           tga_int16;
 
+
+typedef size_t(*TGAWriteBytes_t)(void* src, size_t read, void* user);
 typedef size_t(*TGAReadBytes_t)(void* dst, size_t read, void* user);
 typedef size_t(*TGASeekBytes_t)(void* dst, size_t read, void* user);
 typedef void*(*TGAmalloc_t)(size_t size);
@@ -9,6 +10,7 @@ typedef void*(*TGAfree_t)(void* ptr);
 
 typedef TGADataReaderFuncs
 {
+    TGAWriteBytes_t writer_;
     TGAReadBytes_t  reader_;
     TGASeekBytes_t  seek_;
     TGAmalloc_t     mallocFunc_;
@@ -26,6 +28,9 @@ typedef TGAImageInfo
 
 typedef TGAImage TGAImage_t;
 
-TGAImage_t*         TGACreateImage(TGADataReaderFuncs* funcs);
+TGAImage_t*         TGACreateImage(TGADataReaderFuncs* funcs, void* user);
 void                TGADestroyImage(TGAImage* image);
 TGAImageInfo_t*     TGAGetImageInfo(TGAImage* image);
+int                 TGASetImageInfo(TGAImage* image);
+int                 TGASetImageData(tga_byte* src, size_t size);
+int                 TGAReadImage(TGAImage* image, void* dst, size_t maxSize);
