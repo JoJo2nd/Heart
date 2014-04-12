603226dbe2b6f38197916bd4b89dc8f65e3ebe98
james_moran <jojo.2nd.jm@gmail.com>
2014-04-12 22:52:23 +0100

    Updated material proto buffer to use enums rather that raw numbers for render state. Workaround for build process for included files not being copied early enough.

--- build/deploy_scripts/create_pb_lite_file.bat
+++ build/deploy_scripts/create_pb_lite_file.bat
@@ -1,7 +1,11 @@
 rem @echo off
 
-echo //Auto generated file. DO NOT EDIT! > "%3"
-echo option optimize_for=LITE_RUNTIME; >> "%3"
-pushd "%1"
-type "%2" >> "%3"
+set WORKING_DIR="%1"
+set DEST_FILE=%3
+set SRC_FILE=%2
+
+echo //Auto generated file. DO NOT EDIT! > %DEST_FILE%
+echo option optimize_for=LITE_RUNTIME; >> %DEST_FILE%
+pushd %WORKING_DIR%
+type %SRC_FILE% >> %DEST_FILE%
 popd
