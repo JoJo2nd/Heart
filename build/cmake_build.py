588086311c6a8f0f51481b7140c08a4c56f22b2f
james_moran <jojo.2nd.jm@gmail.com>
2014-06-04 19:54:30 +0100

    Swapped bat script for python script. Python protobuf script correctly spots and copies imports.

--- build/cmake_build.py
+++ build/cmake_build.py
@@ -20,15 +20,16 @@ class Unbuffered(object):
 
 sys.stdout = Unbuffered(sys.stdout)
 
-def checkForNewSource(in_path):
+def checkForNewSource(in_paths):
     exclude_directories = ['.svn','.git']
     full_paths = []
-    for root, dirs, files in walk(in_path):
-        for excluded_dir in exclude_directories: 
-            if excluded_dir in dirs:
-                dirs.remove(excluded_dir)
-        for file in files:
-            full_paths.append(realpath(path_join(root, file))+'\n')
+    for in_path in in_paths:
+        for root, dirs, files in walk(in_path):
+            for excluded_dir in exclude_directories: 
+                if excluded_dir in dirs:
+                    dirs.remove(excluded_dir)
+            for file in files:
+                full_paths.append(realpath(path_join(root, file))+'\n')
     try:
         build_list = open('build.timestamp.txt', 'r')
     except:
@@ -38,55 +39,59 @@ def checkForNewSource(in_path):
     build_list.close()
     return full_paths != prev_paths
 
-def writeBuildTimestamp(in_path):
+def writeBuildTimestamp(in_paths):
     exclude_directories = ['.svn','.git']
     full_paths = []
-    for root, dirs, files in walk(in_path):
-        for excluded_dir in exclude_directories: 
-            if excluded_dir in dirs:
-                dirs.remove(excluded_dir)
-        for file in files:
-            full_paths.append(realpath(path_join(root, file))+'\n')
+    for in_path in in_paths:
+        for root, dirs, files in walk(in_path):
+            for excluded_dir in exclude_directories: 
+                if excluded_dir in dirs:
+                    dirs.remove(excluded_dir)
+            for file in files:
+                full_paths.append(realpath(path_join(root, file))+'\n')
     build_list = open('build.timestamp.txt', 'w')
     build_list.writelines(full_paths)
     build_list.close()
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog="cmake_build",description='Helper script for cmake compiles')
+    parser.add_argument('-r','--root', help='root source directory')
+    parser.add_argument('-p','--path', help='cmake build directory')
 
-parser = argparse.ArgumentParser(prog="cmake_build",description='Helper script for cmake compiles')
-parser.add_argument('-r','--root', help='root source directory')
-parser.add_argument('-p','--path', help='cmake build directory')
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    heart_src_path = realpath(path_join(args.path,'../heart/src'))
+    build_script_path = realpath(path_join(args.path,'../build'))
+    heart_output_path = realpath(path_join(args.path,'../heart/src'))
 
-heart_src_path = realpath(path_join(args.path,'../heart/src'))
-heart_output_path = realpath(path_join(args.path,'../heart/src'))
+    print('Checking project is up-to-date...',)
+    project_build_dirs = [heart_src_path, build_script_path]
+    if checkForNewSource(project_build_dirs):
+        print('re-generating project.')
+        subprocess.call(['python', 'c_cpp_unity_creator.py', 
+            '-d', heart_src_path, 
+            '-x', 'unix', 
+            '-x', 'posix',
+            '-e', '.+\.(h|hpp|gitignore|py|lua|txt|inl)',
+            '-e', '.+(.unity[\d*].cpp)', 
+            '-e', '.+([U|u]nity.cpp)', 
+            '-o', heart_output_path, '-n', 'heart'])
+        subprocess.call(["cmake", args.path, '-DUNITY_BUILD=ON'])
+        writeBuildTimestamp(project_build_dirs)
+    else:
+        print('project is up-to-date')
 
-print('Checking project is up-to-date...',)
-if checkForNewSource(heart_src_path):
-    print('re-generating project.')
-    subprocess.call(['python', 'c_cpp_unity_creator.py', 
-        '-d', heart_src_path, 
-        '-x', 'unix', 
-        '-x', 'posix',
-        '-e', '.+\.(h|hpp|gitignore|py|lua|txt|inl)',
-        '-e', '.+(.unity[\d*].cpp)', 
-        '-e', '.+([U|u]nity.cpp)', 
-        '-o', heart_output_path, '-n', 'heart'])
-    subprocess.call(["cmake", args.path, '-DUNITY_BUILD=ON'])
-    writeBuildTimestamp(heart_src_path)
-else:
-    print('project is up-to-date')
+    print('Starting build...')
 
-print('Starting build...')
-
-proc = subprocess.Popen(["cmake", "--build", args.path], stdout=subprocess.PIPE, universal_newlines=True)
-while not proc.poll():
-    line = proc.stdout.readline()
-    if line != '' and not line.isspace():
-        #todo fix to handle forward slash...?
-        match = re_match('(\d+>\s*)(?:\.\.\\\\)*(.*)\((\d+)\)(.*)', line)
-        if match:
-            corrected_path = realpath(path_join(args.root, match.group(2)))
-            print(match.group(1)+corrected_path+'('+match.group(3)+')'+match.group(4))
-        else:
-            print(line.strip())
+    # subprocess.call(["cmake", "--build", args.path])
+    proc = subprocess.Popen(["cmake", "--build", args.path], stdout=subprocess.PIPE, universal_newlines=True)
+    while proc.poll() is None:
+        line = proc.stdout.readline()
+        if line != '' and not line.isspace():
+            #todo fix to handle forward slash...?
+            match = re_match('(\d+>\s*)(?:\.\.\\\\)*(.*)\((\d+)\)(.*)', line)
+            if match:
+                corrected_path = realpath(path_join(args.root, match.group(2)))
+                print(match.group(1)+corrected_path+'('+match.group(3)+')'+match.group(4))
+            else:
+                print(line.strip())
--- build/deploy_scripts/copy_create_proto_files.py
+++ build/deploy_scripts/copy_create_proto_files.py
@@ -1,23 +1,37 @@
 #! /usr/bin/python
 
 import argparse
-from os.path import join, realpath
+from os.path import join, realpath, split
 from re import match as re_match
 import os
 
-parser = argparse.ArgumentParser(prog='copy_create_proto_files',description='Helper script for google protobuf files')
-parser.add_argument('-s','--src', help='.proto source directory')
-parser.add_argument('-d','--dest', help='.proto destination directory')
+def writeFile(root, in_file, dest):
+    in_file = str(in_file)
+    out_includes = []
+    input_file = open(realpath(join(root, in_file)), 'r')
+    output_file = open(realpath(join(dest, in_file)), 'wt')
+    output_file.write('//Auto generated file. DO NOT EDIT!\n')
+    output_file.write('option optimize_for=LITE_RUNTIME;\n')
+    for line in input_file.readlines():
+        match = re_match('\s*?import\s+?\"(.+?)\"', line)
+        if match:
+            out_includes.append(match.group(1))
+        output_file.write(line)
+    input_file.close()
+    output_file.close()
+    return out_includes
 
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='copy_create_proto_files',description='Helper script for google protobuf files')
+    parser.add_argument('-s','--src', help='.proto source file')
+    parser.add_argument('-d','--dest', help='.proto destination directory')
 
-for root, dirs, files in os.walk(args.src):
-    for file in files:
-        if (re_match(file, '.*?\.proto')):
-            input_file = open(realpath(root, file), 'r')
-            output_file = open(realpath(args.dest, file), 'wt')
-            output_file.write('//Auto generated file. DO NOT EDIT!\n')
-            output_file.write('option optimize_for=LITE_RUNTIME;\n')
-            output_file.writelines(input_file.readlines())
-            input_file.close()
-            output_file.close()
+    args = parser.parse_args()
+    print(args.src)
+    proto_root, proto_file = split(args.src)
+    files_to_process = [proto_file]
+
+    while len(files_to_process) != 0:
+        files_to_process.extend(writeFile(proto_root, files_to_process[0], args.dest))
+        files_to_process = files_to_process[1:]
+
